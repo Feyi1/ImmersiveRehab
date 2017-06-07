72c7d0aed60be452ef83dcbec5cd5e7dc64bd60b
Feyi1 <feyi.okelowo@gmail.com>
2017-06-07 13:20:11 -0700

    Add files via upload
    More files

---
+++ Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+
+public class Menu : MonoBehaviour
+{
+    public void NewGameLoad(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+        //Application.LoadLevel(sceneonLoad);
+
+    }
+       
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
